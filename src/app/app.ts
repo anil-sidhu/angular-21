15ff6c207ae250947984548685e6f8b169396744
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-12-17 00:38:29 +0530

    worked on clicked

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,25 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Profile } from './profile/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Profile],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  // template:`<h1>inline Template {{name}}<h1>`,
   styleUrl: './app.css'
 })
 export class App {
- name="Anil sidhu"
- email="anil@test.com"
- getName(a:number,b:number){
-  return a+b;
- }
+
+  count = 0;
+  counter(action: string) {
+    if (action == 'minus') {
+      this.count>0 && this.count--
+    } else {
+      this.count++
+    }
+// this.showUserName();
+  }
+
+  showUserName(){
+    alert("Hello")
+  }
 }
- 
