c4bdf58b74f0c2547bfa6d1e73150d85f40d526f
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-12-23 18:44:29 +0530

    done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,15 @@
 import { Component, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserData } from './user-data/user-data';
+import { AdminData } from './admin-data/admin-data';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,UserData,AdminData],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  // styleUrl: './app.css'
+  styleUrls:["./app.css","./common.css"]
 })
 export class App {
- user = signal("anil")
- users= signal(['anil','sam','peter'])
 
-
- updateSignal(){
-  this.user.set("peter")
-  this.users.update((preVal)=>[...preVal,'bruce'])
- }
 }
