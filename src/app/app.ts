281d9cacaac206994ebfb0e110530b2e21a9fe5f
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-12-25 23:31:28 +0530

    working in switch case

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,12 +9,9 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  users=signal(["Anil","Sam","Peter","Bruce"])
-  usersDetail=signal([
-    { id:1, name:"Anil", surname:"sidhu", email:"anil@test.com"},
-    { id:2, name:"sam", surname:"singh", email:"sam@test.com"},
-    { id:3, name:"Peter", surname:"parker", email:"peter@test.com"},
-    { id:4, name:"bruce", surname:"wayen", email:"bruce@test.com"},
-
-  ])
+ status=signal("notStarted")
+ handleSwitch(event:Event){
+  const target = event.target as HTMLSelectElement
+  this.status.set(target.value)
+ }
 }
