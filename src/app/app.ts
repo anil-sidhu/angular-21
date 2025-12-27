99a72789dfb471ec143d1a1c27c0bdf6992222be
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-12-28 02:09:39 +0530

    done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,30 +1,17 @@
 
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  tasks = signal([
-    { id: 0, title: "Team lunch", completed: false }
-  ])
-  title = signal('')
-
-  addTask() {
-    if (this.title()) {
-      this.tasks.update((item) => (
-        [...item, { id: this.tasks().length, title: this.title(), completed: false }]
-      ))
-      this.title.set('');
-    }
-
-  }
-  deleteTask(id: number) {
-    this.tasks.update((tasks) => tasks.filter((task) => task.id != id))
-  }
+isLogin= signal(true)
+users=signal(["anil","same","peter","bruce"])
+pColor="red"
 
 }
