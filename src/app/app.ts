82d526815174f8a8f4f9d15d85ea932961708e71
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-12-28 19:51:53 +0530

    done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,19 @@
 
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-
+import { Component, signal } from '@angular/core';
+import { Child } from './child/child';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule,Child],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+users=signal(['anil','peter','sam','bruce'])
+newUser=signal('')
+
+addNewUser(){
+  this.users.update((data)=>([...data, this.newUser()]))
+}
 }
