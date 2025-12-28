8320fc52ed002a2f146031275c7754d879fb606b
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-12-28 15:26:28 +0530

    done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { SearchBox } from './search-box/search-box';
 
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule,SearchBox],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  nums=signal([0,1,2,3,4,5])
 }
---
+++ src/app/name-box/name-box.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-name-box',
+  imports: [],
+  template: `
+    <p>
+      name-box works!
+    </p>
+  `,
+  styleUrl: './name-box.css',
+})
+export class NameBox {
+
+}
