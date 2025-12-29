dc3960627a519dff1bf1998b99042df09cb0cfa9
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-12-30 00:50:21 +0530

    piples

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports:[CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  title=signal("code step by step")
+  name=signal("");
 
+  amount=10000;
+  date="8-24-2025";
+  mobile="samsung"
+  today= new Date()
+  user=signal({name:'anil',age:29,email:"code@test.com"})
 }
  
