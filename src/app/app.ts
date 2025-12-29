fb044c319236a938a3ac95c96ea29a8f7c0b0436
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-12-29 18:48:53 +0530

    piples

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,13 @@
 
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { DisplayCount } from './display-count/display-count';
-import { ControlCount } from './control-count/control-count';
-
+import { Component, signal } from '@angular/core';
 @Component({
   selector: 'app-root',
-  imports: [CommonModule,DisplayCount,ControlCount],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
 
 }
+ 
