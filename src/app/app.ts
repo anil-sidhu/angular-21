584cc064ea3e0af394ad1d0376273f7b7c6b0de2
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-12-29 15:31:15 +0530

    done with child to parent data

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,15 +5,28 @@ import { Child } from './child/child';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule,Child],
+  imports: [CommonModule, Child],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-users=signal(['anil','peter','sam','bruce'])
-newUser=signal('')
+  users = signal(['anil', 'peter', 'sam', 'bruce'])
+  newUser = signal('')
+  selectUserName = signal('')
 
-addNewUser(){
-  this.users.update((data)=>([...data, this.newUser()]))
-}
+  addNewUser() {
+    this.users.update((data) => ([...data, this.newUser()]))
+  }
+
+  selectedUser(name: string) {
+
+    this.selectUserName.set(name)
+
+  }
+  deleteUser(name: string) {
+    console.log(name);
+    this.users.update((data)=>data.filter((item)=>item!=name))
+  }
+
+
 }
