d2edd502908e4c43be86a1b444e5f0ba83364037
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-12-29 17:51:17 +0530

    done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,32 +1,15 @@
 
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
-import { Child } from './child/child';
+import { Component } from '@angular/core';
+import { DisplayCount } from './display-count/display-count';
+import { ControlCount } from './control-count/control-count';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, Child],
+  imports: [CommonModule,DisplayCount,ControlCount],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  users = signal(['anil', 'peter', 'sam', 'bruce'])
-  newUser = signal('')
-  selectUserName = signal('')
-
-  addNewUser() {
-    this.users.update((data) => ([...data, this.newUser()]))
-  }
-
-  selectedUser(name: string) {
-
-    this.selectUserName.set(name)
-
-  }
-  deleteUser(name: string) {
-    console.log(name);
-    this.users.update((data)=>data.filter((item)=>item!=name))
-  }
-
 
 }
---
+++ src/app/control-count/control-count.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Counter } from '../store/counter';
+
+@Component({
+  selector: 'app-control-count',
+  imports: [],
+  templateUrl: './control-count.html',
+  styleUrl: './control-count.css',
+})
+export class ControlCount {
+  
+constructor(public state:Counter){}
+}
