2b2cef6a608e88cdc204915ccd8433f2221384e6
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-12-30 15:24:33 +0530

    done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,18 @@
 
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
-import { TrimTextPipe } from './custom-pipe/trim-text-pipe';
-import { CurrencyConvertorPipe } from './custom-pipe/currency-convertor-pipe';
+import { Component, effect, signal } from '@angular/core';
 @Component({
   selector: 'app-root',
-  imports: [CommonModule,TrimTextPipe,CurrencyConvertorPipe],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  title=signal("Code step by step")
-  name=signal("My name is anil sidhu")
-  amount=signal(20)
-
+count=signal(0)
+constructor(){
+  effect(()=>{
+    console.log("count val is :");
+    
+  })
+}
 }
