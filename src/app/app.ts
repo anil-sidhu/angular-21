6227180839503fbe38dfb351a67e228542b8f002
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-12-30 19:02:34 +0530

    worked on angular routing with header

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet,RouterLink],
+  imports: [CommonModule, RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
