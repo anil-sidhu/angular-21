9be1c50b7cb21f0ebad0ff6689cd92c794171c3f
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-12-31 16:40:47 +0530

    done

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+  constructor(public route:Router){}
+userData=signal({name:'anil',age:29,id:1})
 
+goToProfile(){
+  this.route.navigate(['profile'],{
+    queryParams:{
+      id:10,
+      name:'peter',
+      age:40
+    }
+  })
+
+}
 }
