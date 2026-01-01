e74142534ad23984a8571effaf6b169882aa5e25
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2026-01-01 16:40:26 +0530

    done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,21 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
-import { User } from './pages/user/user';
 import { Profile } from './pages/profile/profile';
 import { Setting } from './pages/setting/setting';
+import { Admin } from './pages/admin/admin';
 
 
 export const routes: Routes = [
 {path:'',component:Home},
-{path:'user',component:User,
+{path:'admin', loadComponent:()=>import('./pages/admin/admin').then((c)=>c.Admin),},
+{path:'user',
+    // component:User,
+    loadComponent:()=>import('./pages/user/user').then((c)=>c.User),
+
     children:[
         {path:'',redirectTo:"profile",pathMatch:"full"},
-        {path:'profile', component:Profile},
-        {path:'setting',component:Setting},
+        {path:'profile', loadComponent:()=>import('./pages/profile/profile').then((c)=>c.Profile)},
+        {path:'setting',loadComponent:()=>import('./pages/setting/setting').then((c)=>c.Setting)},
 
     ]},
 
