eaac8917380c0f8fee6a71a99fd916e8e09fd781
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2026-01-02 18:58:04 +0530

    worked on dynamic forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,5 @@
 import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
-import { Profile } from './pages/profile/profile';
-import { Setting } from './pages/setting/setting';
-import { Admin } from './pages/admin/admin';
-
 
 export const routes: Routes = [
-{path:'',component:Home},
-{path:'admin', loadComponent:()=>import('./pages/admin/admin').then((c)=>c.Admin),},
-{path:'user',
-    // component:User,
-    loadComponent:()=>import('./pages/user/user').then((c)=>c.User),
-
-    children:[
-        {path:'',redirectTo:"profile",pathMatch:"full"},
-        {path:'profile', loadComponent:()=>import('./pages/profile/profile').then((c)=>c.Profile)},
-        {path:'setting',loadComponent:()=>import('./pages/setting/setting').then((c)=>c.Setting)},
-
-    ]},
 
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,23 @@
 
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, ViewChild, ViewContainerRef} from '@angular/core';
+import { UserDetails } from './user-details/user-details';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule,RouterOutlet,RouterLink],
+  imports: [CommonModule,UserDetails],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  @ViewChild("container",{read: ViewContainerRef})
+  container: ViewContainerRef | undefined
 
+  async loadUserDetails(){
+    this.container?.clear()
+
+    const {UserDetails}= await import('./user-details/user-details')
+    this.container?.createComponent(UserDetails)
+    
+  }
 }
