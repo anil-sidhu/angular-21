6acacd2e9a542a646125bf4ed0da144f7ca64f7f
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2026-01-04 19:44:52 +0530

    worked on reactive forms

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,25 @@
 
 import { CommonModule } from '@angular/common';
 import { Component} from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+email=new FormControl("default@test.com")
+password=new FormControl("12345")
 
+login(){
+  console.log(this.email.value,this.password.value);
+}
+
+reset(){
+  this.email.setValue("");
+  this.password.setValue("");
+
+}
 
 }
