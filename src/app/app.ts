3513cfd50f37d14c2d954859825715df8423469f
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2026-01-04 16:48:10 +0530

    done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,23 +1,13 @@
 
 import { CommonModule } from '@angular/common';
-import { Component, ViewChild, ViewContainerRef} from '@angular/core';
-import { UserDetails } from './user-details/user-details';
-
+import { Component} from '@angular/core';
 @Component({
   selector: 'app-root',
-  imports: [CommonModule,UserDetails],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  @ViewChild("container",{read: ViewContainerRef})
-  container: ViewContainerRef | undefined
 
-  async loadUserDetails(){
-    this.container?.clear()
 
-    const {UserDetails}= await import('./user-details/user-details')
-    this.container?.createComponent(UserDetails)
-    
-  }
 }
