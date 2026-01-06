d4e6f31afbcbfffeb6a2cead8ca775d8e4d0f84e
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2026-01-06 21:58:50 +0530

    worked

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,40 +1,27 @@
 
 import { CommonModule } from '@angular/common';
-import { Component} from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, signal } from '@angular/core';
+import { Field, form } from '@angular/forms/signals';
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, Field],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-loginForm=new FormGroup({
-  name:new FormControl('',[Validators.required,Validators.maxLength(20)]),
-  email:new FormControl('',[Validators.required,Validators.email]),
-  password:new FormControl('',[Validators.required,Validators.minLength(5)]),
-})
-
-get name(){
-  return this.loginForm.get("name")
-}
-
-get email(){
-  return this.loginForm.get("email")
-}
-get password(){
-  return this.loginForm.get("password")
-}
-
-handleProfile(){
-  console.log(this.loginForm.value);
-}
-reset(){
-  this.loginForm.setValue({
-    name:'',
-    password:'',
-    email:''
+  loginModel = signal({
+    email: 'abc',
+    password: '123'
   })
-}
+  loginForm = form(this.loginModel)
+  login() {
+    console.log(this.loginForm.email().value());
+    console.log(this.loginForm.password().value());
 
+  }
+  reset() {
+    this.loginForm.email().value.set('')
+   this.loginForm.password().value.set('')
+
+  }
 }
