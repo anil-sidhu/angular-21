8ea9249d7db3daa14c3bb98503ec29be1c32bb6c
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2026-01-07 14:05:11 +0530

    done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { Field, form } from '@angular/forms/signals';
+import { email, Field, form, maxLength, minLength, required } from '@angular/forms/signals';
 @Component({
   selector: 'app-root',
   imports: [CommonModule, Field],
@@ -10,10 +10,19 @@ import { Field, form } from '@angular/forms/signals';
 })
 export class App {
   loginModel = signal({
-    email: 'abc',
-    password: '123'
+    email: '',
+    password: ''
   })
-  loginForm = form(this.loginModel)
+  loginForm = form(this.loginModel,(field)=>{
+    required(field.email,{message:"please enter email address"});
+    minLength(field.email,5,{message:"please enter valid email"});
+    email(field.email,{message:"please enter valid email address"});
+
+    required(field.password,{message:"please enter a password email"});
+    minLength(field.password,5,{message:"please enter valid password"});
+    maxLength(field.password,10,{message:"please enter valid password"});
+
+  })
   login() {
     console.log(this.loginForm.email().value());
     console.log(this.loginForm.password().value());
