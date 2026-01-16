dff148767eb458c5e31fd29333eea669f871d3c9
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2026-01-16 14:36:24 +0530

    worked on angular material ui

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
 
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [MatButtonModule, MatBadgeModule, MatCardModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
